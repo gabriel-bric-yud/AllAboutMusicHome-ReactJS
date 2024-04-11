5dfe32e62f20e837f8cab174ae99fa12529f2518
gbabyblue <114004755+gbabyblue@users.noreply.github.com>
2024-04-10 22:51:38 -0400

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes,Route} from 'react-router-dom';
+import {Router,Routes,Route} from 'react-router-dom';
 import './App.css';
 import Home from './Home.js';
 import ContactUs from './ContactUs.js';
