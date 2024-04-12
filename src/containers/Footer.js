9211f6e93bdb9e003d323437485ce1463ebf7fd6
Aaron Meese <ajmeese7@gmail.com>
2024-04-12 13:46:44 +0000

    feat: clickable phone number

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
         <div className="address">
           <a href="https://maps.app.goo.gl/iRYiVKHdG7XyHLDV6" >8928 Waltham Woods Road, Baltimore, Maryland 21234</a>
         </div>
-        <div id = "phoneNumber">
+        <a id="phoneNumber" href="tel:410-665-6600">
           410-665-6600
-        </div>
+        </a>
       </div>
     </div>
   )
